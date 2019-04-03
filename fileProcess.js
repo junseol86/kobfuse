a7a583c66928c54c88ecddfd08ed9513eafdc013
KFace_Hyunmin <junseol86@gmail.com>
2019-04-03 09:58:12 +0900

    Edit readdir to readdirSync

--- fileProcess.js
+++ fileProcess.js
@@ -15,25 +15,10 @@ let submitProg = document.querySelector('.submitProgress');
 
 FileProcess.prototype.init = function () {
   const fp = this;
+  while (fp.dirsToPrc.length > 0) {
+    let dirToPrc = fp.dirsToPrc.pop();
+    fp.fs.readdirSync(dirToPrc).forEach((inDir) => {
 
-  fp.processDirs();
-}
-
-// 모든 내부 폴더들을 처리
-FileProcess.prototype.processDirs = function () {
-  const fp = this;
-  if (fp.dirsToPrc.length === 0) return;
-
-  console.log("+++++");
-  console.log(fp.dirsToPrc);
-  let dirToPrc = fp.dirsToPrc.pop();
-  console.log(dirToPrc);
-  console.log(fp.dirsToPrc);
-  fp.fs.readdir(dirToPrc, (err, inDirs) => {
-    if (err) {
-      fp.submitProg.innerHTML = err;
-    }
-    inDirs.forEach((inDir, idx) => {
       let indName = dirToPrc + '/' + inDir;
       if (fp.fs.lstatSync(indName).isDirectory()) {
         fp.dirsToPrc.push(indName);
@@ -41,13 +26,42 @@ FileProcess.prototype.processDirs = function () {
       } else {
         fp.procFileCount++;
       }
-      fp.submitProg.innerHTML = fp.procFileCount + '개 파일, ' + fp.procDirCount + '개 폴더 처리됨';
-      // console.log(indName);
-      if (idx == inDirs.length - 1) {
-        fp.processDirs();
-      }
+      setTimeout(() => {
+        fp.submitProg.innerHTML = fp.procFileCount + '개 파일, ' + fp.procDirCount + '개 폴더 처리됨';
+      }, 0);
+      console.log(indName);
+
     });
-  });
+  }
+
+  // fp.processDirs();
 }
+
+// 모든 내부 폴더들을 처리
+// FileProcess.prototype.processDirs = function () {
+//   const fp = this;
+//   if (fp.dirsToPrc.length === 0) return;
+
+//   let dirToPrc = fp.dirsToPrc.pop();
+//   fp.fs.readdir(dirToPrc, (err, inDirs) => {
+//     if (err) {
+//       fp.submitProg.innerHTML = err;
+//     }
+//     inDirs.forEach((inDir, idx) => {
+//       let indName = dirToPrc + '/' + inDir;
+//       if (fp.fs.lstatSync(indName).isDirectory()) {
+//         fp.dirsToPrc.push(indName);
+//         fp.procDirCount++;
+//       } else {
+//         fp.procFileCount++;
+//       }
+//       fp.submitProg.innerHTML = fp.procFileCount + '개 파일, ' + fp.procDirCount + '개 폴더 처리됨';
+//       console.log(indName);
+//       if (idx == inDirs.length - 1) {
+//         fp.processDirs();
+//       }
+//     });
+//   });
+// }
 
 module.exports = FileProcess;
