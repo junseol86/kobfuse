00063592cf5329e56148bc178864a603d1adafa0
KFace_Hyunmin <junseol86@gmail.com>
2019-04-03 11:29:42 +0900

    Add cookie

--- renderer.js
+++ renderer.js
@@ -1,7 +1,24 @@
 const remote = require('electron').remote;
 const dialog = remote.dialog;
+const session = remote.session;
 const fs = remote.require('fs');
 const fileProcess = require('./fileProcess');
+
+session.defaultSession.cookies.get({url: 'https://www.ddhouse.co.kr'}, (error, cookies) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(cookies);
+  cookies.forEach((cookie, idx) => {
+    if (cookie.name === 'fromDir') {
+      setFromDir(cookie.value);
+    }
+    if (cookie.name === 'toDir') {
+      setToDir(cookie.value);
+    }
+  })
+})
 
 let fromDir = null;
 let toDir = null;
@@ -9,18 +26,35 @@ let toDir = null;
 let submitProg = document.querySelector('.submitProgress');
 
 document.querySelector('.dirSelect.from').addEventListener('click', () => {
-  fromDir = dialog.showOpenDialog({
+  setFromDir(dialog.showOpenDialog({
       properties: ['openDirectory']
-  })[0];
-  document.querySelector('.dirStr.from').innerHTML = fromDir;
+  })[0]);
+})
+document.querySelector('.dirSelect.to').addEventListener('click', () => {
+  setToDir(dialog.showOpenDialog({
+      properties: ['openDirectory']
+  })[0]);
 })
 
-document.querySelector('.dirSelect.to').addEventListener('click', () => {
-  toDir = dialog.showOpenDialog({
-      properties: ['openDirectory']
-  })[0];
+
+function setFromDir (fd) {
+  fromDir = fd;
+  document.querySelector('.dirStr.from').innerHTML = fromDir;
+  session.defaultSession.cookies.set({
+    url: 'https://www.ddhouse.co.kr', name: 'fromDir', value: fromDir
+  }, (error) => {
+    if (error) console.log(error)
+  })
+}
+function setToDir (td) {
+  toDir = td;
   document.querySelector('.dirStr.to').innerHTML = toDir;
-})
+  session.defaultSession.cookies.set({
+    url: 'https://www.ddhouse.co.kr', name: 'toDir', value: toDir
+  }, (error) => {
+    if (error) console.log(error)
+  })
+}
 
 document.querySelector('.submitBtn').addEventListener('click', () => {
   if (fromDir === null || toDir === null) {
